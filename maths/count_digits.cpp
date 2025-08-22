26194397caa51830d50077d66b52338a303be160
Krishna <krishna161062@gmail.com>
2025-08-22 21:02:10 +0530

    count_digit_log_approach

--- maths/count_digits.cpp
+++ maths/count_digits.cpp
@@ -1,12 +1,9 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int countDigits(int n) {
-    int count = 0;
-    while(n != 0){
-        count++;
-        n = n/10;
-    }
+    int count = log10(n) + 1;
     return count;
 }
 
